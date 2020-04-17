5809c30a6280a4a6a844549ab84b243f73ead1d6
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2020-04-16 21:32:51 -0400

    add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,63 @@
 import React from 'react'
 
 const Header = () => {
+  const [isActive, setisActive] = React.useState(false);
+
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <a className="navbar-item" href="#">
-          <p className="is-size-4">vicinity</p>
+          <strong class="is-size-4">vicinity</strong>
         </a>
-
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a onClick={() => {
+          setisActive(!isActive);
+        }}
+          role="button"
+          className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar-main">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div className="navbar-end">
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">
-              Home
-        </a>
-
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
+      <div id="navbar-main"
+        className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+        <div className="navbar-start">
+          {/* <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="text" placeholder="Search" />
             </div>
-          </div>
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">Log in</a>
+            <div class="control">
+              <a class="button is-primary">Search</a>
+            </div>
+          </div> */}
+        </div>
+        <div className="navbar-end is-size-8">
+          <a className="navbar-item">Home</a>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+
+            </div>
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">Sign up</a>
+                <a className="button is-light">Log in</a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </nav>
+
+    </nav >
   )
 }
 
